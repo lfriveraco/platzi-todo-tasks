61cc91aa4853c56b50607d91108ba2caf68ef5af
Luis Fernando Rivera Cortés <luisfernandoriveracortes@gmail.com>
2022-06-18 20:40:34 -0500

    logica de busc, compl, y eliminar

--- src/App.js
+++ src/App.js
@@ -6,23 +6,65 @@ import { TodoList } from './TodoList';
 import { TodoSearch } from './TodoSearch';
 // import './App.css';
 
-const todos = [
+const defaultTodos = [
   { text: 'Cortar cebolla', completed: true },
   { text: 'Tomar el curso de Intro a react', completed: false },
-  { text: 'Llorar con la Llorona', completed: false },
+  { text: 'Llorar con la Llorona', completed: true },
+  { text: 'LALALALALAL', completed: false },
 ]
 
 function App(props) {
+  const [todos, setTodos] = React.useState(defaultTodos);
+  const [searchValue, setSearchValue] = React.useState('');
+
+  const completedTodos = todos.filter(todo => todo.completed).length;
+  const totalTodos = todos.length;
+
+  let searchedTodos = [];
+
+  if(!searchValue.length > 0) {
+    searchedTodos = todos;
+  } else {
+    searchedTodos = todos.filter(todo => {
+      const todoText =  todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+      return todoText.includes(searchText);
+    });
+  }
+
+  const completeTodo = (text) => {
+    const todoIndex = todos.findIndex(todo => todo.text === text);
+    const newTodos = [...todos];
+    newTodos[todoIndex].completed = true;
+    setTodos(newTodos);
+  }
+
+  const deleteTodo = (text) => {
+    const todoIndex = todos.findIndex(todo => todo.text === text);
+    const newTodos = [...todos];
+    newTodos.splice(todoIndex,1);
+    setTodos(newTodos);
+  }
+
   return (
     <React.Fragment>
-      <TodoCounter />
-      <TodoSearch />
+      <TodoCounter
+        total={totalTodos}
+        completed={completedTodos}
+      />
+      <TodoSearch
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+      />
       <TodoList>
-        {todos.map(todo => (
-          <TodoItem 
+        {searchedTodos.map(todo => (
+          <TodoItem
             key={todo.text}
             text={todo.text}
-            completed={todo.completed} />
+            completed={todo.completed}
+            onComplete={() => completeTodo(todo.text)}
+            onDelete={() => deleteTodo(todo.text)}
+          />
         ))}
       </TodoList>
 
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import './TodoSearch.css';
 
-function TodoSearch() {
+function TodoSearch({searchValue, setSearchValue}) {
+
+  const onSearchValueChange = (event) => {
+    setSearchValue(event.target.value)
+    console.log(event.target.value);
+  }
   return (
-    <input className='TodoSearch' placeholder="Cebolla" />
+    <input
+      className='TodoSearch'
+      placeholder="Cebolla"
+      value={searchValue}
+      onChange={onSearchValueChange}
+    />
   );
 }
 
